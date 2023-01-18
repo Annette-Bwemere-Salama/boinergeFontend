40d237197dfe173495b5e86e41a502f3da193ff5
Annette Bwemere <bwemereannette@gmail.com>
2023-01-18 09:49:37 +0200

    feat: squellette de la page login

---
+++ src/COMPONENT/login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./login.css"
+function Login() {
+  return (
+    <div className='container'>
+      <div className='block1'>img</div>
+      <div className='block2'>login</div>
+    
+
+    </div>
+      
+
+  )
+}
+
+export default Login

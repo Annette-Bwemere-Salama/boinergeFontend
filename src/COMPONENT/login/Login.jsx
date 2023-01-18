d30bf898e334a55464cab294b3f3ffe86ab03b95
Annette Bwemere <bwemereannette@gmail.com>
2023-01-18 12:16:35 +0200

    feat: loginReact

--- src/COMPONENT/login/Login.jsx
+++ src/COMPONENT/login/Login.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 import "./login.css"
 function Login() {
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    // suite continuité
+    alert(mail, password
+      ,"Fait avec succes ${mail}")
+  }
   return (
     <div className='container'>
       <div className='blockImage'>img</div>
       <div className='blockFormulaire'>
           <div className='form'>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <label>Entez votre mail:
-                  <input type="mail" placeholder='ex:bwemereannette@gmail.com'/>
                 </label>
-<br></br>
-
+                  <input type="email" value={mail} placeholder='ex:bwemereannette@gmail.com' className='mail' 
+                  onChange={(e)=> setMail(e.target.value)}
+                  />
                 <label>Entez votre Mot de pass:
-                  <input type="password" placeholder='ex:.......'/>
                 </label> 
-                <button><input type="submit" /></button>
+                  <input type="password" value={password} placeholder='ex:.......' className='password'
+                  onChange={(e)=> setPassword(e.target.value)}
+                  />
+                <button>Login</button>
               </form>
           </div>
         </div>

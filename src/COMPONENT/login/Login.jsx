e1e27f28424166e0434dd04a405628631b3fb74d
Annette Bwemere <bwemereannette@gmail.com>
2023-02-21 21:32:16 +0200

    style:input

--- src/COMPONENT/login/Login.jsx
+++ src/COMPONENT/login/Login.jsx
@@ -1,63 +1,64 @@
 import React from 'react'
 import { useState } from 'react'
-// import Register from "../register/Register.jsx"
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 
 import "./login.css"
 function Login() {
 
-  const navigate = useNavigate(null);
+  // const navigate = useNavigate(null);
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     try {
       // let user = await
       // suite continuité
       alert(mail, password
-        ,"Fait avec succes ${mail}")
+        , "Fait avec succes ${mail}")
     } catch (error) {
       // setState({error})
-      console.log({error})
+      console.log({ error })
     }
-   
+
   }
   return (
     <div className='container'>
       <div className='blockImage'><h1>BoinergeT</h1></div>
       <div className='blockFormulaire'>
-          <div className='form'>
-              <form onSubmit={handleSubmit}>
-                <label>
-                  Entez votre mail
-                </label>
-                  <input type="email" value={mail} placeholder='ex:bwemereannette@gmail.com' className='mail' 
-                  onChange={(e)=> setMail(e.target.value)}
-                  />
-                <label>
-                  
-                  Entez votre Mot de pass
-                </label> 
-                <input type="passw ord" value={password} placeholder='ex:....' className='password'
-                  onChange={(e)=> setPassword(e.target.value)}
-                />
-                  <div>
-                  <button className='loginButon'>Login</button>
-                  <button  className='registerButton' onClick={()=>{
-                    navigate("/Register")
-                  }}>Register
-                  </button>
-                  </div>
-              </form>
-           <a href=''>Mot de passe Oublier?</a>          
-          </div>
+        <div className='form'>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Entez votre mail
+            </label>
+            <input type="email" value={mail} placeholder='ex:bwemereannette@gmail.com' className='mail'
+              onChange={(e) => setMail(e.target.value)}
+            />
+            <label>
+
+              Entez votre Mot de pass
+            </label>
+            <input type="passw ord" value={password} placeholder='ex:....' className='password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div>
+              <button className='loginButon'>Login</button>
+              <button className='registerButton'
+              //  onClick={() => {
+              //   navigate("/Register")
+              // }}
+              >Register
+              </button>
+            </div>
+          </form>
+          <a href=''>Mot de passe Oublier?</a>
         </div>
-  
+      </div>
+
     </div>
-      
+
 
   )
 }

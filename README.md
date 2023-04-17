7c3386e6913c86d3243dc37f778c01f0aa8e3e25
Annette Bwemere <bwemereannette@gmail.com>
2023-04-17 14:57:23 +0200

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# boinergeFontend
+# Logiciel de gestion d'église
+
+Ce logiciel permet de gérer les activités quotidiennes d'une église.
+
+## Fonctionnalités
+
+- Gestion des membres : enregistrement des informations des membres, suivi des contributions et des dons
+- Gestion des événements : planification et organisation des événements de l'église
+- Gestion financière : suivi des dépenses et des revenus de l'église
+- Rapports : génération de rapports sur les activités de l'église
+
+## Installation
+
+1. Téléchargez la dernière version du logiciel à partir de la page des [releases](https://github.com/Annette-Bwemere-Salama/boinergeFontend).
+2. Décompressez l'archive téléchargée.
+3. Exécutez le fichier d'installation et suivez les instructions à l'écran.
+
+## Utilisation
+
+Pour utiliser le logiciel, ouvrez-le et connectez-vous avec vos identifiants. Vous pouvez ensuite accéder aux différentes fonctionnalités à partir du menu principal.
+
+## Contribution
+
+Si vous souhaitez contribuer au développement du logiciel, veuillez consulter les [instructions de contribution](CONTRIBUTING.md).
+
+## Licence
+
+Ce logiciel est distribué sous la licence MIT. Voir le fichier [LICENSE](LICENSE) pour plus d'informations.
+
+
+## Contributeur
+
+[Annette B](https://github.com/Annette-Bwemere-Salama).
--- src/COMPONENT/login/Login.jsx
+++ src/COMPONENT/login/Login.jsx
@@ -25,38 +25,61 @@ function Login() {
 
   }
   return (
-    <div className='container'>
-      <div className='blockImage'><h1>BoinergeT</h1></div>
-      <div className='blockFormulaire'>
-        <div className='form'>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Entez votre mail
-            </label>
-            <input type="email" value={mail} placeholder='ex:bwemereannette@gmail.com' className='mail'
-              onChange={(e) => setMail(e.target.value)}
-            />
-            <label>
+    // <div className='container'>
+    //   <div className='blockImage'><h1>BoinergeT</h1></div>
+    //   <div className='blockFormulaire'>
+    //     <div className='form'>
+    //       <form onSubmit={handleSubmit}>
+    //         <label>
+    //           Entez votre mail
+    //         </label>
+    //         <input type="email" value={mail} placeholder='ex:bwemereannette@gmail.com' className='mail'
+    //           onChange={(e) => setMail(e.target.value)}
+    //         />
+    //         <label>
 
-              Entez votre Mot de pass
-            </label>
-            <input type="passw ord" value={password} placeholder='ex:....' className='password'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div>
-              <button className='loginButon'>Login</button>
-              <button className='registerButton'
-              //  onClick={() => {
-              //   navigate("/Register")
-              // }}
-              >Register
-              </button>
+    //           Entez votre Mot de pass
+    //         </label>
+    //         <input type="passw ord" value={password} placeholder='ex:....' className='password'
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //         <div>
+    //           <button className='loginButon'>Login</button>
+    //           <button className='registerButton'
+    //           //  onClick={() => {
+    //           //   navigate("/Register")
+    //           // }}
+    //           >Register
+    //           </button>
+    //         </div>
+    //       </form>
+    //       <a href=''>Mot de passe Oublier?</a>
+    //     </div>
+    //   </div>
+
+    // </div>
+    <div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style="background-image:url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')">
+      <div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+        <div class="text-white">
+          <div class="mb-8 flex flex-col items-center">
+            <img src="https://www.logo.wine/a/logo/Instagram/Instagram-Glyph-Coor-Logo.wine.svg" width="150" alt="" srcset="" />
+            <h1 class="mb-2 text-2xl">BoinergeTa</h1>
+            <span class="text-gray-300">Enter Login Details</span>
+          </div>
+          <form action="#">
+            <div class="mb-4 text-lg">
+              <input class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="text" name="name" placeholder="id@email.com" />
+            </div>
+
+            <div class="mb-4 text-lg">
+              <input class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="Password" name="name" placeholder="*********" />
+            </div>
+            <div class="mt-8 flex justify-center text-lg text-black">
+              <button type="submit" class="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Login</button>
             </div>
           </form>
-          <a href=''>Mot de passe Oublier?</a>
         </div>
       </div>
-
     </div>
 
 

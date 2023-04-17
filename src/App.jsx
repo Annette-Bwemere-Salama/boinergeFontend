14a037a6bbedb5032f9959a29ba81a4016e489ed
Annette Bwemere <bwemereannette@gmail.com>
2023-04-17 16:35:14 +0200

    resolv conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from "react"
 import './App.css'
 import Login from './COMPONENT/login/Login';
-import Register from "../../frontend/src/COMPONENT/register/Register";
-import Dashboard from './COMPONENT/dashboard/Dashboard';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Register from "../../frontend/src/COMPONENT/register/Register";
+// import Dashboard from './COMPONENT/dashboard/Dashboard';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css"
 
 function App() {
